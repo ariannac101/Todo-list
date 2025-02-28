c3ad0533e7b023bb140243a9fdc9da1bb9694528
ariannac101 <ariannac101@nycstudents.net>
2025-02-28 16:29:32 +0000

    changes 2

--- client/script.js
+++ client/script.js
@@ -1,8 +1,23 @@
+import { response } from "express";
+
 // Get DOM HTML Elements
+ let todo= getElementById("todoList")
+ let input= getElementById("input");
+ let form= getElementById("form")
 
 // Fetch all todos when page loads
 async function fetchTodos() {
     // add your code
+try {
+    const response = await fetch(" ");
+    const todos = await response.json;
+    displayTodos(todos);
+
+} 
+catch(error){
+    console.error("error fetching todo", error);
+}
+
     }
     
     // Display todos in the list
