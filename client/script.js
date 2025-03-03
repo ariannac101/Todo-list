60e6f6892c19b90a23345f070e685516331b6408
ariannac101 <ariannac101@nycstudents.net>
2025-03-03 16:15:57 +0000

    changes3

--- client/script.js
+++ client/script.js
@@ -1,9 +1,8 @@
-import { response } from "express";
-
 // Get DOM HTML Elements
- let todo= getElementById("todoList")
- let input= getElementById("input");
- let form= getElementById("form")
+ 
+ const todoList= document.getElementById("todoList");
+ const todoInput= document.getElementById("todoInput");
+ const todoForm= document.getElementById("todoForm");
 
 // Fetch all todos when page loads
 async function fetchTodos() {
@@ -15,19 +14,40 @@ try {
 
 } 
 catch(error){
-    console.error("error fetching todo", error);
+    console.error("error fetching todos", error);
 }
 
     }
     
     // Display todos in the list
     function displayTodos(todos) {
-    // add your code
+    todoList.innerHTML = "";
+    todos.forEach(todo => {
+        const li= document.createElement("li");
+        li.textContent = todo.title
+        todoList.appendChild(li);
+
+    })
     }
     
     // Handle form submission
     todoForm.addEventListener('submit', async (e) => {
-    // add your code  
+    e.preventDefault();
+    // gets the value from the imput field and removes spaces using trim.
+    const newTodo= todoInput.value.trim()
+    if(newTodo){
+        // display todo added to the console.
+        console.log("New Todo:", newTodo)
+        todoInput.value= "";
+        const li = document.createElement("li")
+        li.textContent = newTodo;
+        todoList.appendChild(li);
+
+    }else{
+        alert("PLEASE ENTER TODO !")
+    }
+
+
     });
     
     // Load todos when page loads
